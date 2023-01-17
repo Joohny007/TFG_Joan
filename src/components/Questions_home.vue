7beab240c71dfca4d2428866dee759613c21c50b
Joohny007 <joan.gonzalez.gravi1b@gmail.com>
2023-01-17 19:04:31 +0100

    nextQuestion

--- src/components/Questions_home.vue
+++ src/components/Questions_home.vue
@@ -1,5 +1,11 @@
 <template>
-  <form id="form_box">
+  <div id="p1" class="blue-square "  style="width:200px; height: 200px; background-color: aqua;"></div>
+  <div id="p2" class="red-square "  style="width:200px; height: 200px; background-color: tomato; display:none"></div>
+  <button type="button" @click="nextQuestion()">PEGAME</button>
+
+  
+  
+  <!-- <form id="form_box">
     <div class="row">
       <div class="col-12 form-group">
         <label class="col-form-label col-form-label-lg">Nombre Completo <span class="text-danger">*</span></label>
@@ -18,13 +24,19 @@
         <input type="text" class="form-control form control-lg">
       </div>
     </div>
-  </form>
+  </form> -->
 </template>
   
   <script>
   
   export default {
     name: 'Questions_home',
+    methods: {
+    nextQuestion: function(){
+      document.getElementById("p1").style.display="none"
+      document.getElementById("p2").style.display="block"
+    }
+  }
 
   }
   </script>
