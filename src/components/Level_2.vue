1bcd9f122a6447d8aef4e9506fb3985d0b0f9834
Joohny007 <joan.gonzalez.gravi1b@gmail.com>
2023-02-28 20:46:50 +0100

    level_2

--- src/components/Level_2.vue
+++ src/components/Level_2.vue
@@ -3,7 +3,32 @@
 
     <div class="row">
         <div class="column">
-            <img v-bind:src="require('./images/cansancio.png')" alt="">
+            <input id="estrés" type="image" v-bind:src="require('../images/estrés.jpg')" alt="" style="width: 100%;" @click="mark('estrés')">
+            <p>Estrés</p>
+        </div>
+        <div class="column">
+            <input id="cansancio" type="image" v-bind:src="require('../images/cansancio.png')" alt="" style="width: 100%;" @click="mark('cansancio')">
+            <p>Cansancio</p>
+        </div>
+        <div class="column">
+            <input id="depresión" type="image" v-bind:src="require('../images/estrés.jpg')" alt="" style="width: 100%;" @click="mark('depresión')">
+            <p>Depresión</p>
+        </div>
+        <div class="column">
+            <input id="mal_humor" type="image" v-bind:src="require('../images/mal humor.jpg')" alt="" style="width: 100%;" @click="mark('mal_humor')">
+            <p>Mal humor</p>
+        </div>
+        <div class="column">
+            <input id="nervios" type="image" v-bind:src="require('../images/estrés.jpg')" alt="" style="width: 100%;" @click="mark('nervios')">
+            <p>Nervios</p>
+        </div>
+        <div class="column">
+            <input id="hambre" type="image" v-bind:src="require('../images/hambre.jpg')" alt="" style="width: 100%;" @click="mark('hambre')">
+            <p>Hambre</p>
+        </div>
+        <div class="column">
+            <input id="malasnotas" type="image" v-bind:src="require('../images/malas notas.jpg')" alt="" style="width: 100%;" @click="mark('malasnotas')">
+            <p>Malas notas</p>
         </div>
     </div>
     <button id="comprobar_level2" class="buttonQuestion" style="margin: auto;" @click="verification2(); nextLevel('comprobar_level2', 'next_level2')">Comprobar</button>
@@ -14,6 +39,21 @@
     export default {
         name: "Level_2",
         methods: {
+            mark: function(id) {
+                if (document.getElementById(id).style.border == "10px solid cyan"){
+                    document.getElementById(id).style.border = "0px solid cyan";
+                }else{
+                    document.getElementById(id).style.border = "10px solid cyan";
+                    document.getElementById(id).style.borderRadius = "5px";
+                    document.getElementById(id).style.padding = "5px";
+                }
+            },
+
+            verification2: function() {
+                
+            },
+
+
             nextLevel: function(id, nextId){
             document.getElementById(id).style.display="none"
             document.getElementById(nextId).style.display="block"
